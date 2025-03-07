6a83ea651383e39b284c4e3922ee54b5d80ed79a
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2025-03-06 21:47:27 -0800

    feat: disabled tab closure prompts

--- config/keys.lua
+++ config/keys.lua
@@ -5,7 +5,20 @@ local util = require("util")
 local M = {}
 
 M.keys = {
-    { key = "F11", action = act.ToggleFullScreen }
+    {
+        key = "F11",
+        action = act.ToggleFullScreen
+    },
+    {
+        key = "w",
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentTab({ confirm = false })
+    },
+    {
+        key = "w",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.CloseCurrentTab({ confirm = false })
+    }
 }
 
 function M.register(config)
